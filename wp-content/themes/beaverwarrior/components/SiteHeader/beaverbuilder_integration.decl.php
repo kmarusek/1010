9099ef79c39c6272504d735cf1925bcec0c2d82e
David Wendt <david.w@huemordesigns.com>
2017-10-30 15:18:27 -0400

    Kill fl-header/fl-footer

--- wp-content/themes/beaverwarrior/components/SiteHeader/beaverbuilder_integration.decl.php
+++ wp-content/themes/beaverwarrior/components/SiteHeader/beaverbuilder_integration.decl.php
@@ -6,3 +6,9 @@ function beaver_warrior_remove_bb_nav_menus() {
     unregister_nav_menu('footer');
 }
 add_action('after_setup_theme', 'beaver_warrior_remove_bb_nav_menus', 11);
+
+function beaver_warrior_remove_bb_settings($wp_customize) {
+    $wp_customize->remove_section("fl-header");
+    $wp_customize->remove_section("fl-footer");
+}
+add_action('customize_register', 'beaver_warrior_remove_bb_settings', 11);
