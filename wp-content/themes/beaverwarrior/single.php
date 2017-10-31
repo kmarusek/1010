867dfcb8be3ea466614e086d0818efd184911f8f
David Wendt <david.w@huemordesigns.com>
2017-10-31 12:21:20 -0400

    Override SKW blog inner page atop BB blog inner page

---
+++ wp-content/themes/beaverwarrior/single.php
@@ -0,0 +1,10 @@
+<?php print get_template_component('HTML'); ?>
+<?php print get_template_component('Page'); ?>
+<?php print get_template_component('SiteHeader'); ?>
+<?php print get_template_component('HeroHeader'); ?>
+
+<?php print get_template_layout('Article'); ?>
+
+<?php print get_template_component('SiteFooter'); ?>
+<?php print get_template_component('Page', 'end'); ?>
+<?php print get_template_component('HTML', 'end'); ?>
