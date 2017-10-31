ecdfdf59b3ce2b73999e79a5c8a204f72b652682
David Wendt <david.w@huemordesigns.com>
2017-10-31 16:54:08 -0400

    Remove the content DIV, it's unnecessary and intereferes with SKW styling.

--- wp-content/themes/beaverwarrior/layouts/Article/Article.php
+++ wp-content/themes/beaverwarrior/layouts/Article/Article.php
@@ -16,39 +16,37 @@ $linkedin = "https://www.linkedin.com/shareArticle?url=" . $url . "&title=" . $t
 
 ?>
 
-<div class="fl-content <?php FLTheme::content_class(); ?>">
-    <?php if ( have_posts() ) { ?>
-        <?php while (have_posts()) {
-            the_post(); ?>
-            <?php do_action('fl_before_post'); ?>
+<?php if ( have_posts() ) { ?>
+    <?php while (have_posts()) {
+        the_post(); ?>
+        <?php do_action('fl_before_post'); ?>
 
-            <main <?php post_class("fl-post Article Article--full"); ?> id="fl-post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">
-                <div class="Article-body">
-                    <?php do_action('fl_before_post_content'); ?>
+        <main <?php post_class("fl-post Article Article--full"); ?> id="fl-post-<?php the_ID(); ?>" itemscope="itemscope" itemtype="http://schema.org/BlogPosting">
+            <div class="Article-body">
+                <?php do_action('fl_before_post_content'); ?>
 
-                    <div class="fl-post-content clearfix" itemprop="text">
-                        <?php the_content(); ?>
-                    </div>
-
-                    <?php do_action('fl_after_post_content'); ?>
+                <div class="fl-post-content clearfix" itemprop="text">
+                    <?php the_content(); ?>
                 </div>
-                <nav class="Article-share">
-                    <h2><?php echo __('Share this post', 'skeleton_warrior'); ?></h2>
-                    <ul class="Article-share_buttons">
-                        <li>
-                            <a href="<?php echo $facebook; ?>" class="Article-share_link Article-share_link--facebook" target="_blank"><span>Facebook</span></a>
-                            <a href="<?php echo $twitter; ?>" class="Article-share_link Article-share_link--twitter" target="_blank"><span>Twitter</span></a>
-                            <a href="<?php echo $pinterest; ?>" class="Article-share_link Article-share_link--pinterest" target="_blank"><span>Pinterest</span></a>
-                            <a href="<?php echo $linkedin; ?>" class="Article-share_link Article-share_link--linkedin" target="_blank"><span>Linkedin</span></a>
-                        </li>
-                    </ul>
-                </nav>
-                <div class="Article-comments">
-                    <?php comments_template(); ?>
-                </div>
-            </main>
 
-            <?php do_action('fl_after_post'); ?>
-        <?php }
-    } ?>
-</div>
+                <?php do_action('fl_after_post_content'); ?>
+            </div>
+            <nav class="Article-share">
+                <h2><?php echo __('Share this post', 'skeleton_warrior'); ?></h2>
+                <ul class="Article-share_buttons">
+                    <li>
+                        <a href="<?php echo $facebook; ?>" class="Article-share_link Article-share_link--facebook" target="_blank"><span>Facebook</span></a>
+                        <a href="<?php echo $twitter; ?>" class="Article-share_link Article-share_link--twitter" target="_blank"><span>Twitter</span></a>
+                        <a href="<?php echo $pinterest; ?>" class="Article-share_link Article-share_link--pinterest" target="_blank"><span>Pinterest</span></a>
+                        <a href="<?php echo $linkedin; ?>" class="Article-share_link Article-share_link--linkedin" target="_blank"><span>Linkedin</span></a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="Article-comments">
+                <?php comments_template(); ?>
+            </div>
+        </main>
+
+        <?php do_action('fl_after_post'); ?>
+    <?php }
+} ?>
