45d20a4f041e936b31015987751026c5de1d1845
David Wendt <david.w@huemordesigns.com>
2017-10-31 16:00:35 -0400

    Use BB article template for a bit

--- wp-content/themes/beaverwarrior/single.php
+++ wp-content/themes/beaverwarrior/single.php
@@ -2,7 +2,17 @@
 <?php print get_template_component('Page'); ?>
 <?php print get_template_component('SiteHeader'); ?>
 
-<?php print get_template_layout('Article'); ?>
+<div class="container">
+	<div class="row">
+
+		<div class="fl-content <?php FLTheme::content_class(); ?>">
+			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+				<?php get_template_part('content', 'single'); ?>
+			<?php endwhile; endif; ?>
+		</div>
+
+	</div>
+</div>
 
 <?php print get_template_component('SiteFooter'); ?>
 <?php print get_template_component('Page', 'end'); ?>
