73a6c282cccf877ab64099ab634b388ce44f4f2a
David Wendt <david.w@huemordesigns.com>
2017-11-07 16:41:23 -0500

    Bunch up the search results into columns

--- wp-content/themes/beaverwarrior/layouts/SearchResults/SearchResults.php
+++ wp-content/themes/beaverwarrior/layouts/SearchResults/SearchResults.php
@@ -1,18 +1,21 @@
-<main>
-    <?php if ( have_posts() ) : ?>
-        <?php while (have_posts()) {
-            the_post(); ?>
+<main class="SearchResults">
+    <div class="SearchResults-content">
+        <?php if ( have_posts() ) : ?>
+            <?php while (have_posts()) {
+                the_post(); ?>
+                <article class="SearchResults-item">
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_content(); ?>
+                </article>
+            <?php }
 
-            <h1><?php the_title(); ?></h1>
-            <?php the_content(); ?>
-        <?php }
-
-        // Previous/next page navigation.
-        the_posts_pagination( array(
-            'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-            'next_text'          => __( 'Next page', 'twentyfifteen' ),
-            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-        ) );
-    endif;
-    ?>
+            // Previous/next page navigation.
+            the_posts_pagination( array(
+                'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+                'next_text'          => __( 'Next page', 'twentyfifteen' ),
+                'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+            ) );
+        endif;
+        ?>
+    </div>
 </main>
