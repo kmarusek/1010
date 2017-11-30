2966ad251421976712e0eda9bfac0ba8287605ee
David Wendt <david.w@huemordesigns.com>
2017-11-30 13:25:15 -0500

    Reorganize the Header section

--- wp-content/themes/beaverwarrior/components/SiteHeader/beaverbuilder_integration.decl.php
+++ wp-content/themes/beaverwarrior/components/SiteHeader/beaverbuilder_integration.decl.php
@@ -10,7 +10,19 @@ add_action('after_setup_theme', 'beaver_warrior_remove_bb_nav_menus', 11);
 function beaver_warrior_remove_bb_settings() {
     global $wp_customize;
 
-    //$wp_customize->remove_panel("fl-header");
-    //$wp_customize->remove_panel("fl-footer");
+    $wp_customize->remove_section("fl-topbar-layout");
+    $wp_customize->remove_section("fl-header-layout");
+    $wp_customize->remove_section("fl-header-logo");
+    $wp_customize->remove_section("fl-nav-layout");
+
+    $wp_customize->get_section("title_tagline")->panel = 'fl-header';
+    $wp_customize->get_section("title_tagline")->priority = 0;
+
+    @$wp_customize->get_section("sidebar-widgets-site_header_banner")->panel = 'fl-header';
+    @$wp_customize->get_section("sidebar-widgets-site_header_banner")->priority = 4;
+    @$wp_customize->get_section("sidebar-widgets-site_header_primary_navigation")->panel = 'fl-header';
+    @$wp_customize->get_section("sidebar-widgets-site_header_primary_navigation")->priority = 4;
+
+    $wp_customize->get_panel("nav_menus")->priority = 1;
 }
 add_action('customize_register', 'beaver_warrior_remove_bb_settings', 11);
