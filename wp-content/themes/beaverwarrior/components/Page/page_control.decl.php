6c19ec3038d0dd2fd5bdfd0007120c7b07838e3d
David Wendt <david.w@huemordesigns.com>
2018-01-10 16:49:31 -0500

    Resurface the page border control from Skeleton Warrior

---
+++ wp-content/themes/beaverwarrior/components/Page/page_control.decl.php
@@ -0,0 +1,38 @@
+<?php
+
+function skeleton_warrior_register_page_border($wp_customize) {
+    $wp_customize->add_setting('skeleton_warrior_pagecolor', array());
+
+    $wp_customize->add_control(new WP_Customize_Control(
+        $wp_customize,
+        'skeleton_warrior_pagecolor',
+        array(
+            'label' => __('Default Page Color', 'skeleton_warrior'),
+            'section' => 'colors',
+            'settings' => 'skeleton_warrior_pagecolor',
+            'type' => 'radio',
+            'choices' => array(
+                'light' => __('Light Page', 'skeleton_warrior'),
+                'dark' => __('Dark Page', 'skeleton_warrior'),
+            )
+        )
+    ));
+
+    $wp_customize->add_setting('skeleton_warrior_pageborder', array());
+
+    $wp_customize->add_control(new WP_Customize_Control(
+        $wp_customize,
+        'skeleton_warrior_pageborder',
+        array(
+            'label' => __('Page Border', 'skeleton_warrior'),
+            'section' => 'colors',
+            'settings' => 'skeleton_warrior_pageborder',
+            'type' => 'radio',
+            'choices' => array(
+                'border' => __('Add a page border', "skeleton_warrior"),
+                'no_border' => __('Omit page border', "skeleton_warrior"),
+            )
+        )
+    ));
+}
+add_action('customize_register', 'skeleton_warrior_register_page_border');
