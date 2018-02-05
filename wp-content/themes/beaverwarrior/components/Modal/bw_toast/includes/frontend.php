2c91fb976461578f2f739dfe7cf99a02d30bb65b
David Wendt <david.w@huemordesigns.com>
2018-02-05 13:41:33 -0500

    Add a module to trigger the Offcanvas/Phylactery style of modal.

---
+++ wp-content/themes/beaverwarrior/components/Modal/bw_toast/includes/frontend.php
@@ -0,0 +1,14 @@
+<?php
+
+$idbase = uniqid();
+
+?>
+
+<button type="button" class="Button" data-target="#<?php echo $idbase; ?>" data-toggle="offcanvas" data-toggle-options="nohover"><?php echo $module->get_button_content($settings); ?></button>
+<div class="Modal is-Offcanvas--closed" id="<?php echo $idbase; ?>">
+    <button type="button" class="Modal-close" data-dismiss="offcanvas">X</button>
+    <div class="Modal-content">
+        <?php echo $module->get_modal_content( $settings ); ?>
+    </div>
+</div>
+<div class="Offcanvas-backdrop is-Offcanvas--backdrop_inactive" data-offcanvas-backdrop="1" data-offcanvas-backdrop-for="<?php echo $idbase; ?>"></div>
