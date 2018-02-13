e2d1fa638d748b030bc8ed3c95f91f3114f8eba3
David Wendt <david.w@huemordesigns.com>
2018-02-13 17:29:39 -0500

    Actually copy and rebuild the input as a button instead of replacing it.

--- wp-content/themes/beaverwarrior/components/FormItem/gravity_forms.decl.php
+++ wp-content/themes/beaverwarrior/components/FormItem/gravity_forms.decl.php
@@ -10,6 +10,26 @@ function skeletonwarrior_gform_pre_render($form) {
 add_filter('gform_pre_render', 'skeletonwarrior_gform_pre_render');
 
 function skeletonwarrior_gform_submit_button($button, $form) {
-    return "<button class='gform_button button FormItem-action' id='gform_submit_button_{$form['id']}'><span>Submit</span></button>";
+    //return "<button class='gform_button button FormItem-action' id='gform_submit_button_{$form['id']}'><span>Submit</span></button>";
+
+    $dom = new DOMDocument();
+    $dom->loadHTML( $button );
+    $input = $dom->getElementsByTagName( 'input' )->item(0);
+    $new_button = $dom->createElement( 'button' );
+    $new_button->appendChild( $dom->createTextNode( $input->getAttribute( 'value' ) ) );
+    $input->removeAttribute( 'value' );
+    foreach( $input->attributes as $attribute ) {
+        $new_button->setAttribute( $attribute->name, $attribute->value );
+    }
+    $input->parentNode->replaceChild( $new_button, $input );
+
+    //Ensure FormItem-action is present.
+    if ($new_button->hasAttribute("class")) {
+        $new_button->setAttribute("class", $new_button->getAttribute("class") . " FormItem-action");
+    } else {
+        $new_button->setAttribute("class", "FormItem-action");
+    }
+
+    return $dom->saveHtml( $new_button );
 }
 add_filter('gform_submit_button', 'skeletonwarrior_gform_submit_button', 10, 2);
