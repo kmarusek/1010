78485c6c2beed6c1159986ed25928df4c6bcc1f6
David Wendt <david.w@huemordesigns.com>
2018-03-08 13:26:06 -0500

    Rudimentary related posts loop

--- wp-content/themes/beaverwarrior/layouts/Article/bw_post_navigation/includes/frontend.php
+++ wp-content/themes/beaverwarrior/layouts/Article/bw_post_navigation/includes/frontend.php
@@ -15,5 +15,31 @@ $args = apply_filters( 'fl_theme_builder_post_nav', array(
 ) );
 the_post_navigation( $args );
 
+// Related posts extension
+$tags = wp_get_post_tags($post->ID);
+
+if ($tags) {
+    $tag_ids = array();
+
+    foreach ($tags as $indiv_tag) {
+        $tag_ids[] = $indiv_tag->term_id;
+    }
+
+    $related_query = new WP_Query(array (
+        'tag__in' => $tag_ids,
+        'post__not_in' => $post->ID,
+        'posts_per_page' => 4,
+        'caller_get_posts' => 1
+    ));
+
+    while ($related_query->have_posts()) {
+        $related_query->the_post();
+
+        ?>
+        <?php the_title(); ?>
+        <?php
+    }
+}
+
 // Reset the global post variable.
 $post = $original_post;
