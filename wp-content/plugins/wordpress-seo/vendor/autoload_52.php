975ce2d07599f84a2d043951081985d36d01101a
David Wendt <david.w@huemordesigns.com>
2018-03-13 20:24:09 +0000

    Update Yoast SEO to version 7.0.3

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit11869edb83267d16bf2de93b62de420d::getLoader();
+return ComposerAutoloaderInit503e08848fb9da26d79854e9fc1ab0e6::getLoader();
