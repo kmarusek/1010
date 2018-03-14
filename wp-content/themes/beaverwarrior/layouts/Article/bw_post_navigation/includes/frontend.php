da06d9f8ac78c6346236c01977819b9a13ec68d9
David Wendt <david.w@huemordesigns.com>
2018-03-14 13:41:48 -0400

    Don't print navigation either

--- wp-content/themes/beaverwarrior/layouts/Article/bw_post_navigation/includes/frontend.php
+++ wp-content/themes/beaverwarrior/layouts/Article/bw_post_navigation/includes/frontend.php
@@ -8,13 +8,6 @@ if ( is_object( $original_post ) && 0 === $original_post->ID && isset( $wp_query
 	$post = $wp_query->post;
 }
 
-$args = apply_filters( 'fl_theme_builder_post_nav', array(
-	'prev_text' => '&larr; %title',
-	'next_text' => '%title &rarr;',
-	'in_same_term' => $settings->in_same_term,
-) );
-the_post_navigation( $args );
-
 // Related posts extension
 $tags = wp_get_post_tags($post->ID);
 
