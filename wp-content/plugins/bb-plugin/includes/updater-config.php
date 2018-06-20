49f29d53384a5b61658792d62bb7cced9282e172
On-Server Developer <on-server-developer@getpantheon.com>
2018-06-20 14:10:54 +0000

    Updated bb-plugin from 2.1.2.4 to 2.1.3.1

--- wp-content/plugins/bb-plugin/includes/updater-config.php
+++ wp-content/plugins/bb-plugin/includes/updater-config.php
@@ -3,7 +3,7 @@
 if ( class_exists( 'FLUpdater' ) ) {
 	FLUpdater::add_product(array(
 		'name'      => 'Beaver Builder Plugin (Agency Version)',
-		'version'   => '2.1.2.4',
+		'version'   => '2.1.3.1',
 		'slug'      => 'bb-plugin',
 		'type'      => 'plugin',
 	));
