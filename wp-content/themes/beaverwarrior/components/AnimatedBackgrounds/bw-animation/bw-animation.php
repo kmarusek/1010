461a0b76558ca90b1df3d0f5abfca82bbfdee09b
David Wendt <david.w@huemordesigns.com>
2019-04-04 17:32:39 -0400

    Add standalone animation block

---
+++ wp-content/themes/beaverwarrior/components/AnimatedBackgrounds/bw-animation/bw-animation.php
@@ -0,0 +1,51 @@
+<?php
+
+class BWAnimation extends FLBuilderModule {
+    public function __construct() {
+        parent::__construct(array(
+            "name" => __("BW Animation", "skeleton-warrior"),
+            "description" => __("Stand-alone animation block", "skeleton-warrior"),
+            "category" => __("Space Station", "skeleton-warrior"),
+            "dir" => get_stylesheet_directory() . "components/AnimatedBackgrounds/bw-animation/",
+            "url" => get_stylesheet_directory_uri() . "components/AnimatedBackgrounds/bw-animation/",
+            "editor_export" => true,
+            "enabled" => true,
+            "icon" => "button.svg"
+        ));
+    }
+}
+
+FLBuilder::register_module("BWAnimation", array(
+    "general" => array(
+        'title' => __("General", 'skeleton-warrior'),
+        'sections' => array(
+            'general' => array(
+                'fields' => array(
+                    'aspect_ratio' => array(
+                        'type' => 'unit',
+                        'label' => __("Aspect ratio", "skeleton-warrior"),
+                        'description' => __("The shape of the image", "skeleton-warrior"),
+                        'slider' => array(
+                            'min' => 0.5,
+                            'max' => 2,
+                            'step' => 0.01,
+                        )
+                    )
+                )
+            )
+        )
+    ),
+    "layers" => array(
+        'title' => __('Layers', 'skeleton-warrior'),
+        'sections' => array(
+            'bw_ab_layer_1' => bw_animated_background_row_settings_layer(1),
+            'bw_ab_layer_2' => bw_animated_background_row_settings_layer(2),
+            'bw_ab_layer_3' => bw_animated_background_row_settings_layer(3),
+            'bw_ab_layer_4' => bw_animated_background_row_settings_layer(4),
+            'bw_ab_layer_5' => bw_animated_background_row_settings_layer(5),
+            'bw_ab_layer_6' => bw_animated_background_row_settings_layer(6),
+            'bw_ab_layer_7' => bw_animated_background_row_settings_layer(7),
+            'bw_ab_layer_8' => bw_animated_background_row_settings_layer(8),
+        )
+    )
+));
--- wp-content/themes/beaverwarrior/components/AnimatedBackgrounds/includes.decl.php
+++ wp-content/themes/beaverwarrior/components/AnimatedBackgrounds/includes.decl.php
@@ -1,3 +1,10 @@
 <?php
 
 require_once __DIR__ . "/animated_background_row/settings.decl.php";
+
+function beaverwarrior_load_AnimatedBackgrounds_modules() {
+    if (class_exists("FLBuilder")) {
+        require_once "bw-animation/bw-animation.php";
+    }
+}
+add_action ('init', "beaverwarrior_load_AnimatedBackgrounds_modules", 15);
