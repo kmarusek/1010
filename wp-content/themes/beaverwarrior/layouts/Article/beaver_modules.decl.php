7adeef78b0e5a4ed280a954e332201b52b7ca06e
Jordan Leven <jordan@huemordesigns.com>
2019-04-19 13:17:04 -0400

    Fixes dependency issue with the Post Navigation module

--- wp-content/themes/beaverwarrior/layouts/Article/beaver_modules.decl.php
+++ wp-content/themes/beaverwarrior/layouts/Article/beaver_modules.decl.php
@@ -1,7 +1,7 @@
 <?php
 
 function beaverwarrior_load_Article_modules() {
-    if (class_exists("FLBuilder")) {
+    if (class_exists("FLBuilder") && class_exists('UABB_Helper')) {
         require_once "bw_post_navigation/bw_post_navigation.php";
     }
 }
