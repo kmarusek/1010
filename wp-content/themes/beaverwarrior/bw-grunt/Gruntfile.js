65fbe4f528180b4193aa0c4fbdd192ed1f3e31e6
Jordan Leven <jordan@huemordesigns.com>
2019-06-11 13:47:03 -0400

    ES6ify the Gruntfile

--- wp-content/themes/beaverwarrior/bw-grunt/Gruntfile.js
+++ wp-content/themes/beaverwarrior/bw-grunt/Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
 
     // An array of our custom modules paths
     // relative to theme
-    let custom_modules_upstream = [
+    const custom_modules_upstream = [
     "/components/Animations/bw-animated-svg/",
     "/components/Page/bw-descriptive-list/",
     "/components/Page/bw-textbox-separator/",
@@ -12,37 +12,37 @@ module.exports = function(grunt) {
     "/components/VideoPlayer/bw-video-player/"
     ],
 
-    custom_modules = get_all_custom_modules( custom_modules_upstream );
+    custom_modules = get_all_custom_modules( custom_modules_upstream )
 
     function get_downstream_custom_modules(){
         // Declare our return
-        let return_array          = [];
+        let return_array          = []
         // Check that our downstream file exists
         if ( grunt.file.exists( 'custom-modules-downstream.json' ) ){
             // Get the contents of the downstream files
-            let downstream_custom_modules_file = grunt.file.readJSON( 'custom-modules-downstream.json' );
+            const downstream_custom_modules_file = grunt.file.readJSON( 'custom-modules-downstream.json' )
             // Get the downstream modules
             if ( downstream_custom_modules_file.custom_modules_downstream !== undefined && downstream_custom_modules_file.custom_modules_downstream.length > 0 ){
                 // Redeclare the return array with the downstream modules
-                return_array = downstream_custom_modules_file.custom_modules_downstream;
+                return_array = downstream_custom_modules_file.custom_modules_downstream
             }
         }
         // Return the array
-        return return_array;
+        return return_array
     }
 
     function get_all_custom_modules( custom_modules_upstream ){
         // Get the downstream custom modules
-        let custom_modules_downstream = get_downstream_custom_modules(),
+        const custom_modules_downstream = get_downstream_custom_modules()
         // By default, our module return is just the upstream modules
-        custom_modules_to_transpile = custom_modules_upstream;
+        let custom_modules_to_transpile = custom_modules_upstream
         // If we have custom downstream modules, then we're in an downstream
         // site and shouldn't transpile upstream files
         if ( custom_modules_downstream.length > 0 ){
             custom_modules_to_transpile = custom_modules_downstream
         }
         // Return the array
-        return custom_modules_to_transpile;
+        return custom_modules_to_transpile
     }
 
     /**
@@ -54,11 +54,11 @@ module.exports = function(grunt) {
      */
      function get_module_name( file_path ){
         // Get the array of the file path
-        let file_path_array = file_path.replace(/\/$/, "").split('/'),
+        const file_path_array = file_path.replace(/\/$/, "").split('/')
         // Get the last item in the array (the name)
-        file_name = file_path_array[file_path_array.length - 1];
+        const file_name = file_path_array[file_path_array.length - 1]
         // Return the file name
-        return file_name;
+        return file_name
     }
 
     /**
@@ -68,16 +68,16 @@ module.exports = function(grunt) {
      */
      function get_less_object(){
         // Declare our return
-        let return_array = {};
+        let return_array = {}
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i];
+            const current_module_path = custom_modules[i]
             // Add this modules CSS
-            return_array['..' + current_module_path + 'css/frontend.css'] = '..' + current_module_path + 'css/frontend.less';
+            return_array['..' + current_module_path + 'css/frontend.css'] = '..' + current_module_path + 'css/frontend.less'
         }
         // Return the array
-        return return_array;
+        return return_array
     }
 
     /**
@@ -87,16 +87,16 @@ module.exports = function(grunt) {
      */
      function get_css_lint_array(){
         // Declare our return
-        let return_array = [];
+        let return_array = []
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i];
+            const current_module_path = custom_modules[i]
             // Add this modules CSS
-            return_array.push( '..' + current_module_path + 'css/frontend.css' );
+            return_array.push( '..' + current_module_path + 'css/frontend.css' )
         }
         // Return the array
-        return return_array;
+        return return_array
     }
 
     /**
@@ -106,13 +106,13 @@ module.exports = function(grunt) {
      */
      function get_css_min_object(){
         // Declare our return
-        let return_array = {};
+        let return_array = {}
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i],
+            const current_module_path = custom_modules[i]
             // Create the array for this item
-            module_object = {
+            const module_object = {
                 files: [{
                     expand: true,
                     cwd: '..' + current_module_path + 'css/',
@@ -120,10 +120,10 @@ module.exports = function(grunt) {
                     dest: '..' + current_module_path + 'css/',
                     ext: '.css'
                 }]
-            };
-            return_array[get_module_name(current_module_path)] = module_object;
+            }
+            return_array[get_module_name(current_module_path)] = module_object
         }
-        return return_array;
+        return return_array
     }
 
     /**
@@ -133,23 +133,23 @@ module.exports = function(grunt) {
      */
      function get_uglify_object(){
         // Declare our return
-        let return_array = {};
+        let return_array = {}
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i],
+            const current_module_path = custom_modules[i]
             // Create the array for this item
-            module_object = {
+            const module_object = {
                 files: [{
                     expand: true,
                     cwd: '..' + current_module_path + 'js/',
                     src: ['**/*.js', '!**/*prebuilt.js','!**/*settings.js'],
                     dest: '..' + current_module_path + 'js/'
                 }]
-            };
-            return_array[get_module_name(current_module_path)] = module_object;
+            }
+            return_array[get_module_name(current_module_path)] = module_object
         }
-        return return_array;
+        return return_array
     }
 
     /**
@@ -159,26 +159,26 @@ module.exports = function(grunt) {
      */
      function get_copy_object(){
         // Declare our return
-        let return_array = {};
+        let return_array = {}
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i],
+            const current_module_path = custom_modules[i]
             // Create the array for this item
-            module_object = {
+            const module_object = {
                 files: [{
                     expand: true,
                     cwd: '..' + current_module_path + 'js/',
                     src: ['**/*.js', '!**/frontend.js'],
                     dest: '..'+ current_module_path + 'js/',
                     rename: function(dest, src) {
-                        return dest + src.replace(/\.prebuilt\.js$/, ".js");
+                        return dest + src.replace(/\.prebuilt\.js$/, ".js")
                     }
                 }]
-            };
-            return_array[get_module_name(current_module_path)] = module_object;
+            }
+            return_array[get_module_name(current_module_path)] = module_object
         }
-        return return_array;
+        return return_array
     }
 
     /**
@@ -188,23 +188,23 @@ module.exports = function(grunt) {
      */
      function get_babel_object(){
         // Declare our return
-        let return_object = {};
+        let return_object = {}
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i],
+            const current_module_path = custom_modules[i]
             // The prebuilt path
-            prebuilt_js_file_path = '..'  + current_module_path + 'js/frontend.prebuilt.js',
+            const prebuilt_js_file_path = '..'  + current_module_path + 'js/frontend.prebuilt.js'
             // The file path
-            frontend_js_file_path = '..'  + current_module_path + 'js/frontend.js';
+            const frontend_js_file_path = '..'  + current_module_path + 'js/frontend.js'
             // Only proceed if the JS file exists
             if ( grunt.file.exists( frontend_js_file_path ) ){
                 // Add the item
-                return_object[ frontend_js_file_path ]  = frontend_js_file_path;
+                return_object[ frontend_js_file_path ]  = frontend_js_file_path
             }
         }
         // Return the array
-        return return_object;
+        return return_object
     }
 
     /**
@@ -214,16 +214,16 @@ module.exports = function(grunt) {
      */
      function get_js_hint_array(){
         // Declare our return
-        let return_array = [];
+        let return_array = []
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current moduke
-            let current_module_path = custom_modules[i];
+            const current_module_path = custom_modules[i]
             // Add this modules CSS
-            return_array.push( '..' + current_module_path + 'js/frontend.prebuilt.js' );
+            return_array.push( '..' + current_module_path + 'js/frontend.prebuilt.js' )
         }
         // Return the array
-        return return_array;
+        return return_array
     }
 
     /**
@@ -234,19 +234,19 @@ module.exports = function(grunt) {
      */
      function get_import_object(){
         // Our return
-        let return_object = {};
+        let return_object = {}
         // Add all of the custom modules
         for ( let i=0; i<custom_modules.length; i++){
             // Get the current module
-            let current_module_path = custom_modules[i],
+            const current_module_path = custom_modules[i]
             // The prebuilt path
-            prebuilt_js_file_path = '..'  + current_module_path + 'js/frontend.prebuilt.js',
+            const prebuilt_js_file_path = '..'  + current_module_path + 'js/frontend.prebuilt.js'
             // The file path
-            frontend_js_file_path = '..'  + current_module_path + 'js/frontend.js';
+            const frontend_js_file_path = '..'  + current_module_path + 'js/frontend.js'
             // Only proceed if the JS file exists
             if ( grunt.file.exists( prebuilt_js_file_path ) ){
                 // Add the item
-                return_object[ frontend_js_file_path ] = prebuilt_js_file_path;
+                return_object[ frontend_js_file_path ] = prebuilt_js_file_path
             }
         }
         // Return the array
@@ -254,22 +254,22 @@ module.exports = function(grunt) {
     }
 
     // Get the LESS object
-    let less_items = get_less_object(),
+    const less_items = get_less_object()
     // Get the uglify items
-    uglify_items   = get_uglify_object(),
+    const uglify_items   = get_uglify_object()
+    uglify_items.options = { mangle: {keep_fnames : true} }
     // Get the CSS lint array
-    css_lint_array = get_css_lint_array(),
+    const css_lint_array = get_css_lint_array()
     // Get the copy object
-    copy_object    = get_copy_object(),
+    const copy_object    = get_copy_object()
     // Get the JS Hint array
-    js_hint_array  = get_js_hint_array(),
+    const js_hint_array  = get_js_hint_array()
     // Get the JS Hint array
-    css_min_object = get_css_min_object();
+    const css_min_object = get_css_min_object()
     // Get the babel object
-    js_babel_object = get_babel_object();
+    const js_babel_object = get_babel_object()
     // Get the import object
-    js_import_object = get_import_object();
-    uglify_items.options = { mangle: {keep_fnames : true} };
+    const js_import_object = get_import_object()
     // The grunt configuration
     grunt.initConfig({
         babel: {
@@ -307,7 +307,7 @@ module.exports = function(grunt) {
                     src: '**/*.scss',
                     dest: '../components',
                     ext: '.less',
-                    rename: function(dest, src) { return dest + '/' + src.replace('_','');}
+                    rename: function(dest, src) { return dest + '/' + src.replace('_','')}
                 },
                 {
                     expand: true,
@@ -315,7 +315,7 @@ module.exports = function(grunt) {
                     src: '**/*.scss',
                     dest: '../layouts',
                     ext: '.less',
-                    rename: function(dest, src) { return dest + '/' + src.replace('_','');}
+                    rename: function(dest, src) { return dest + '/' + src.replace('_','')}
                 }]
             }
         },
@@ -397,24 +397,24 @@ module.exports = function(grunt) {
                 files : js_import_object
             }
         }
-    });
+    })
 
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks("grunt-remove-logging");
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-comment-toggler');
-    grunt.loadNpmTasks('grunt-scss2less');
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-import');
+    grunt.loadNpmTasks('grunt-contrib-less')
+    grunt.loadNpmTasks('grunt-contrib-copy')
+    grunt.loadNpmTasks('grunt-contrib-sass')
+    grunt.loadNpmTasks('grunt-contrib-uglify')
+    grunt.loadNpmTasks('grunt-contrib-cssmin')
+    grunt.loadNpmTasks('grunt-contrib-watch')
+    grunt.loadNpmTasks('grunt-contrib-csslint')
+    grunt.loadNpmTasks("grunt-remove-logging")
+    grunt.loadNpmTasks('grunt-contrib-jshint')
+    grunt.loadNpmTasks('grunt-comment-toggler')
+    grunt.loadNpmTasks('grunt-scss2less')
+    grunt.loadNpmTasks('grunt-babel')
+    grunt.loadNpmTasks('grunt-import')
 
-    grunt.task.registerTask('buildDev',  ['buildCSS', 'buildJS']);
-    grunt.task.registerTask('buildCSS',  ['scss2less', 'less']);
-    grunt.task.registerTask('buildJS',   ['import','babel']);
-    grunt.task.registerTask('buildProd', ['buildJS','uglify','buildCSS','cssmin']);
-};
+    grunt.task.registerTask('buildDev',  ['buildCSS', 'buildJS'])
+    grunt.task.registerTask('buildCSS',  ['scss2less', 'less'])
+    grunt.task.registerTask('buildJS',   ['import','babel'])
+    grunt.task.registerTask('buildProd', ['buildJS','uglify','buildCSS','cssmin'])
+}
