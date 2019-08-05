22040a0beb4017749a08fbe731bf009e1a0b1f9a
On-Server Developer <on-server-developer@getpantheon.com>
2019-08-05 14:22:41 +0000

    Updated redirection from 4.3.1 to 4.3.2

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '4.3.1' );
-define( 'REDIRECTION_BUILD', '76c2e333a0fb0e417d3c1937d0c4045c' );
+define( 'REDIRECTION_VERSION', '4.3.2' );
+define( 'REDIRECTION_BUILD', '0f831a6c0558229b803b83eda31409a3' );
 define( 'REDIRECTION_MIN_WP', '4.6' );
