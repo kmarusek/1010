ec0612c4307dac09d198da4eb82bcd89a050f84d
On-Server Developer <on-server-developer@getpantheon.com>
2020-01-24 15:02:58 +0000

    Updated all-in-one-wp-migration from 7.11 to 7.15

--- wp-content/plugins/all-in-one-wp-migration/lib/model/import/class-ai1wm-import-plugins.php
+++ wp-content/plugins/all-in-one-wp-migration/lib/model/import/class-ai1wm-import-plugins.php
@@ -26,6 +26,60 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	die( 'Kangaroos cannot jump here' );
 }
-?>
 
-<a href="https://servmask.com/help" target="_blank"><?php _e( 'Get Support', AI1WM_PLUGIN_NAME ); ?></a>
+class Ai1wm_Import_Plugins {
+
+	public static function execute( $params, $mysql = null ) {
+		global $wpdb;
+
+		// Set progress
+		Ai1wm_Status::info( __( 'Preparing plugins...', AI1WM_PLUGIN_NAME ) );
+
+		// Get database client
+		if ( is_null( $mysql ) ) {
+			if ( empty( $wpdb->use_mysqli ) ) {
+				$mysql = new Ai1wm_Database_Mysql( $wpdb );
+			} else {
+				$mysql = new Ai1wm_Database_Mysqli( $wpdb );
+			}
+		}
+
+		$tables = $mysql->get_tables();
+
+		// Get base prefix
+		$base_prefix = ai1wm_table_prefix();
+
+		// Get mainsite prefix
+		$mainsite_prefix = ai1wm_table_prefix( 'mainsite' );
+
+		// Check WP sitemeta table exists
+		if ( in_array( "{$mainsite_prefix}sitemeta", $tables ) ) {
+
+			// Get fs_accounts option value (Freemius)
+			$result = $mysql->query( "SELECT meta_value FROM `{$mainsite_prefix}sitemeta` WHERE meta_key = 'fs_accounts'" );
+			if ( $row = $mysql->fetch_assoc( $result ) ) {
+				$fs_accounts = get_option( 'fs_accounts', array() );
+				$meta_value  = maybe_unserialize( $row['meta_value'] );
+
+				// Update fs_accounts option value (Freemius)
+				if ( ( $fs_accounts = array_merge( $fs_accounts, $meta_value ) ) ) {
+					if ( isset( $fs_accounts['users'], $fs_accounts['sites'] ) ) {
+						update_option( 'fs_accounts', $fs_accounts );
+					} else {
+						delete_option( 'fs_accounts' );
+						delete_option( 'fs_dbg_accounts' );
+						delete_option( 'fs_active_plugins' );
+						delete_option( 'fs_api_cache' );
+						delete_option( 'fs_dbg_api_cache' );
+						delete_option( 'fs_debug_mode' );
+					}
+				}
+			}
+		}
+
+		// Set progress
+		Ai1wm_Status::info( __( 'Done preparing plugins.', AI1WM_PLUGIN_NAME ) );
+
+		return $params;
+	}
+}
