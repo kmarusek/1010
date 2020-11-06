e9a0ede242c5f971eb4ea89ea0d40a5054455d0a
Adam Macaulay <adam@huemordesigns.com>
2020-11-06 07:53:01 -0600

    Update wordpress-seo to 15.2.1

--- wp-content/plugins/wordpress-seo/vendor/autoload.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite2f8bba49d1d906710f639a61e99bd9b::getLoader();
+return ComposerAutoloaderInit27d611418e09b72d98b1165f1baaf160::getLoader();
