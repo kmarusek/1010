7f8e716fab81556da94f669dc53cee12ac419d2a
Adam Macaulay <adam@huemordesigns.com>
2021-03-17 07:04:08 -0500

    Update all-in-one 7.38 to 7.39

--- wp-content/plugins/all-in-one-wp-migration/constants.php
+++ wp-content/plugins/all-in-one-wp-migration/constants.php
@@ -35,7 +35,7 @@ define( 'AI1WM_DEBUG', false );
 // ==================
 // = Plugin Version =
 // ==================
-define( 'AI1WM_VERSION', '7.38' );
+define( 'AI1WM_VERSION', '7.39' );
 
 // ===============
 // = Plugin Name =
