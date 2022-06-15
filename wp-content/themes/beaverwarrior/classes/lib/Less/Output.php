cf84bebe26823843c1f3b7f2161bd22db82b1835
David Wendt <david.w@huemordesigns.com>
2022-06-15 13:04:22 -0400

    Update vendored wikimedia/less.php library

--- wp-content/themes/beaverwarrior/classes/lib/Less/Output.php
+++ wp-content/themes/beaverwarrior/classes/lib/Less/Output.php
@@ -6,24 +6,24 @@
  * @package Less
  * @subpackage output
  */
-class Less_Output{
+class Less_Output {
 
 	/**
 	 * Output holder
 	 *
-	 * @var string
+	 * @var string[]
 	 */
-	protected $strs = array();
+	protected $strs = [];
 
 	/**
 	 * Adds a chunk to the stack
 	 *
 	 * @param string $chunk The chunk to output
-	 * @param Less_FileInfo $fileInfo The file information
+	 * @param array $fileInfo The file information
 	 * @param integer $index The index
 	 * @param mixed $mapLines
 	 */
-	public function add($chunk, $fileInfo = null, $index = 0, $mapLines = null){
+	public function add( $chunk, $fileInfo = null, $index = 0, $mapLines = null ) {
 		$this->strs[] = $chunk;
 	}
 
@@ -32,18 +32,17 @@ class Less_Output{
 	 *
 	 * @return boolean
 	 */
-	public function isEmpty(){
-		return count($this->strs) === 0;
+	public function isEmpty() {
+		return count( $this->strs ) === 0;
 	}
-
 
 	/**
 	 * Converts the output to string
 	 *
 	 * @return string
 	 */
-	public function toString(){
-		return implode('',$this->strs);
+	public function toString() {
+		return implode( '', $this->strs );
 	}
 
 }
