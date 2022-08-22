b56b46d4580f27ac7395f8ede8da3976089e4552
smitrevski@servsuite.onmicrosoft.com <smitrevski@servsuite.onmicrosoft.com>
2022-08-22 16:35:02 +0200

    position details module

--- wp-content/themes/beaverwarrior/components/OpenPositionDetails/bw-open-position-details/includes/frontend.php
+++ wp-content/themes/beaverwarrior/components/OpenPositionDetails/bw-open-position-details/includes/frontend.php
@@ -1,6 +1,11 @@
 <?php ?>
     <div class="OpenPositionDetails">
-        <div class="OpenPositionDetails-wrapper">
-            <div class='OpenPositionDetails-details'></div>
+        <div class="OpenPositionDetails-wrapper row">
+            <div class="col-md-6">
+                <div class='OpenPositionDetails-details'></div>
+            </div>
+            <div class="col-md-6">
+                <div class='OpenPositionDetails-form'></div>
+            </div>
         </div>
     </div>
