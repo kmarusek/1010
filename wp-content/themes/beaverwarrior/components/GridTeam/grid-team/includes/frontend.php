0f6af518c8ca93faae22724cde7e24e9033ff610
Adis Alic <aadis@huemor.rocks>
2022-08-25 16:11:12 +0200

    yn module

--- wp-content/themes/beaverwarrior/components/GridTeam/grid-team/includes/frontend.php
+++ wp-content/themes/beaverwarrior/components/GridTeam/grid-team/includes/frontend.php
@@ -20,9 +20,14 @@ $wrapper_classes = [
 		?>
 			<div class="GridTeam-team_member">
 				<div class="GridTeam-member_wrapper">
-					<a data-toggle="modal" data-target="#team_member_modal_<?php echo $n; ?>" href="#" class="GridTeam-panel_link">
+					<?php
+					if ($member->include_modal == 'yes') : ?>
+						<a data-toggle="modal" data-target="#team_member_modal_<?php echo $n; ?>" href="#" class="GridTeam-panel_link">
+						<?php else : ?>
+							<a class="GridTeam-panel_link">
+<?php endif; ?>
 						<div class="GridTeam-image_container">
-							<img src="<?php echo wp_get_attachment_image_url( $member->image, 'medium'); ?>">
+							<img src="<?php echo wp_get_attachment_image_url($member->image, 'medium'); ?>">
 						</div>
 						<div class="GridTeam-text_container">
 							<h4 class='GridTeam-name'>
@@ -32,7 +37,7 @@ $wrapper_classes = [
 								<?php echo $member->position; ?>
 							</h6>
 						</div>
-					</a>
+						</a>
 				</div>
 			</div>
 		<?php
@@ -46,6 +51,7 @@ $wrapper_classes = [
 		$m = preg_replace('/\s+/', '_', $modal->name);
 		$u++;
 	?>
+
 		<div class="GridTeam-modal GridTeam-fade" id="team_member_modal_<?php echo $m; ?>" role="dialog" aria-hidden="true">
 			<div class="GridTeam-modal-dialog GridTeam-modal-dialog-centered modal-lg" role="document">
 				<div class="GridTeam-modal-content">
@@ -71,10 +77,10 @@ $wrapper_classes = [
 										<?php echo $modal->modal_text; ?>
 									</div>
 									<?php
-							if ($member->cta !== '') :
-							?><h4 class='GridTeam-modal-title'>Contact</h4>
-								<a class='GridTeam-cta' href='<?php echo $member->url; ?>'><?php echo $member->cta; ?></a>
-							<?php endif; ?>
+									if ($member->cta !== '') :
+									?><h4 class='GridTeam-modal-title'>Contact</h4>
+										<a class='GridTeam-cta' href='<?php echo $member->url; ?>'><?php echo $member->cta; ?></a>
+									<?php endif; ?>
 								</div>
 							</div>
 						</div>
@@ -84,6 +90,7 @@ $wrapper_classes = [
 				</div>
 			</div>
 		</div>
+
 	<?php
 	endforeach;
 	?>
