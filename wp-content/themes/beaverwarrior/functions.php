705bb1892ce76b3311d531d06d13f5203e4fbd33
Adis Alic <aadis@huemor.rocks>
2022-08-31 11:37:32 +0200

    adding legalUser

--- wp-content/themes/beaverwarrior/functions.php
+++ wp-content/themes/beaverwarrior/functions.php
@@ -671,3 +671,22 @@ function custom_fonts_1010 ( $system_fonts ) {
   
   //Add to Page Builder modules
   add_filter( 'fl_builder_font_families_system', 'custom_fonts_1010' );
+
+
+  /* Create Staff Member User Role */
+add_role(
+    'legalUser', //  System name of the role.
+    __( 'Legal User'  ), // Display name of the role.
+    array(
+        'read'  => true,
+        'delete_posts'  => false,
+        'delete_published_posts' => false,
+        'edit_posts'   => false,
+        'publish_posts' => false,
+        'upload_files'  => false,
+        'edit_pages'  => false,
+        'edit_published_pages'  =>  false,
+        'publish_pages'  => false,
+        'delete_published_pages' => false, // This user will NOT be able to  delete published pages.
+    )
+);
