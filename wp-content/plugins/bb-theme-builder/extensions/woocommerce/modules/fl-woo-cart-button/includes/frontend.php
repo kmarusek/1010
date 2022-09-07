f31486eadab5139aeca2f8b5703f7b1cdf3f2412
Kurt Magay <kmagay@huemordesigns.com>
2022-09-07 12:06:37 +0000

    Update Space Station - Themer Add-On to 1.4.3.2

--- wp-content/plugins/bb-theme-builder/extensions/woocommerce/modules/fl-woo-cart-button/includes/frontend.php
+++ wp-content/plugins/bb-theme-builder/extensions/woocommerce/modules/fl-woo-cart-button/includes/frontend.php
@@ -1,7 +1,22 @@
 <?php
-
-$button = FLPageDataWooCommerce::get_add_to_cart_button();
 global $product;
+$render_woo_cart      = true;
+$button               = FLPageDataWooCommerce::get_add_to_cart_button();
+$show_waitlist_button = true;
+if ( class_exists( 'WC_Wishlists_Plugin' ) && ! empty( $button ) && ! empty( $product ) ) {
+	include $module->dir . 'includes/wishlist-cart-button.php';
+	$render_woo_cart = false;
+} elseif ( $render_woo_cart ) {
+	echo $button;
+	$render_woo_cart = false;
+}
+
+// Render the Waitlist Button if Product Type is not 'grouped'.
+if ( class_exists( 'WooCommerce_Waitlist_Plugin' ) && $show_waitlist_button ) {
+	if ( ! ( empty( $product ) || $product->is_type( 'external' ) || $product->is_type( 'composite' ) || $product->is_type( 'variable' ) ) ) {
+		echo do_shortcode( '[woocommerce_waitlist]' );
+	}
+}
 
 if ( function_exists( 'YITH_YWRAQ_Frontend' ) ) {
 
@@ -9,23 +24,12 @@ if ( function_exists( 'YITH_YWRAQ_Frontend' ) ) {
 		YITH_YWRAQ_Frontend()->hide_add_to_cart_single();
 	}
 
-	echo $button;
+	if ( $render_woo_cart ) {
+		echo $button;
+		$render_woo_cart = false;
+	}
 
 	if ( is_object( $product ) ) {
 		YITH_YWRAQ_Frontend()->add_button_single_page();
-	}
-}
-
-$show_waitlist_button = true;
-if ( class_exists( 'WC_Wishlists_Plugin' ) && ! empty( $button ) && ! empty( $product ) ) {
-	include $module->dir . 'includes/wishlist-cart-button.php';
-} else {
-	echo $button;
-}
-
-// Render the Waitlist Button if Product Type is not 'grouped'.
-if ( class_exists( 'WooCommerce_Waitlist_Plugin' ) && $show_waitlist_button ) {
-	if ( ! ( empty( $product ) || $product->is_type( 'external' ) || $product->is_type( 'composite' ) || $product->is_type( 'variable' ) ) ) {
-		echo do_shortcode( '[woocommerce_waitlist]' );
 	}
 }
