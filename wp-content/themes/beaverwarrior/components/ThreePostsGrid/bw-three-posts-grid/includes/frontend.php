d5942ace80845b5aa441fbcfd3e3c0df3d7fdc50
kmarusek <kevinm@abovemediateam.com>
2022-09-09 14:54:57 -0400

    wrap cpt articles into three post grid for single module

--- wp-content/themes/beaverwarrior/components/ThreePostsGrid/bw-three-posts-grid/includes/frontend.php
+++ wp-content/themes/beaverwarrior/components/ThreePostsGrid/bw-three-posts-grid/includes/frontend.php
@@ -4,7 +4,11 @@ $posts          = $module->getPosts();
 // Get the posts per page
 $posts_per_page = $module->getPostsPerPage();
 
+
+// var_dump($posts);
+
 ?>
+
 <div class="ThreePostsGrid-container container-fluid">
     <div class="row ThreePostsGrid-equalHeight">
     <?php
@@ -17,8 +21,8 @@ $posts_per_page = $module->getPostsPerPage();
         $post_title             = $current_post->post_title;
         // Get the post date
         $post_date              = date('M j Y', strtotime($current_post->post_date));
-
-        
+        // Get the post type
+        $post_type              = $current_post->post_type;
         // Get the post categories string
         $post_categories_string = $module->getPostCategoryString( $post_id );
         // Get the post content and excerpt
@@ -27,9 +31,34 @@ $posts_per_page = $module->getPostsPerPage();
         $post_url               = get_permalink( $post_id );
         // By default, the post classes
         $post_classes = array( 'ThreePostsGrid-post col-xs-12 col-md-6 col-lg-4' );
-    
+
+        if($post_type === 'article'){
         ?>
-        <a class="<?php echo implode(' ', $post_classes ); ?>" href="<?php echo $post_url;?>" data-post-id="<?php echo $post_id;?>">    
+
+        <a class="<?php echo implode(' ', $post_classes ); ?>" href="<?php  the_field('article_link', $post_id);?>" data-post-id="<?php echo $post_id;?>">    
+            <div class="ThreePostsGrid-content">       
+                <div class="ThreePostsGrid-category-container">
+                    <span class="ThreePostsGrid-categories"><?php echo the_field('article_author', $post_id);?></span>
+                </div>
+                <div class="ThreePostsGrid-title-container">
+                    <h4 class="ThreePostsGrid-title"><?php  the_field('article_title', $post_id);?></h4>
+                </div>
+                <div class="ThreePostsGrid-excerpt-container">
+                    <p class="ThreePostsGrid-excerpt"><?php  the_field('article_excerpt', $post_id)?>...</p>
+                </div>
+                <div class="ThreePostsGrid-post-date-container">
+                        <span class="ThreePostsGrid-date"><?php  the_field('article_date', $post_id);?></span>
+                        <span class="ThreePostsGrid-share">Share<i class="ThreePostsGrid-share-icon" style="background-image: url(<?php echo $settings->posts_share_icon_src; ?>);"></i></span>
+                </div>
+            </div>
+
+        </a>
+
+        <?php
+        }
+        elseif($post_type === 'post'){
+        ?>
+            <a class="<?php echo implode(' ', $post_classes ); ?>" href="<?php echo $post_url;?>" data-post-id="<?php echo $post_id;?>">    
             <div class="ThreePostsGrid-content">       
                 <div class="ThreePostsGrid-category-container">
                     <span class="ThreePostsGrid-categories"><?php echo $post_categories_string;?></span>
@@ -47,8 +76,9 @@ $posts_per_page = $module->getPostsPerPage();
             </div>
 
         </a>
-        <?php
+<?php 
     }
+}
     ?>
     </div>
 </div>
