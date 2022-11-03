47851720a574b4012729e878f3f38e736020c62b
kmarusek <kevinm@abovemediateam.com>
2022-11-03 12:35:13 -0400

    dev changes

--- wp-content/themes/beaverwarrior/components/ContentLibrary/bw-content-library/bw-content-library.php
+++ wp-content/themes/beaverwarrior/components/ContentLibrary/bw-content-library/bw-content-library.php
@@ -118,11 +118,6 @@ FLBuilder::register_module(
             'sections' => array(
                 'general' => array(
                     'fields' => array(
-                        'feature_posts_background_image' => array(
-                            'type'      => 'photo',
-                            'label'     => __('Feature Post Background Image', 'fl-builder'),
-                            'show_remove' => true,
-                        ),
                         'webinars_posts_background_image' => array(
                             'type'      => 'photo',
                             'label'     => __('Webinars Background Image', 'fl-builder'),
